7b1c5650b98647e6f7d803d50033a1dc8c4feaef
Beryl-18 <beryla.s@hotmail.com>
2023-07-17 16:11:25 +0400

    Creating the App routes

--- index.js
+++ index.js
@@ -15,5 +15,49 @@ app.get('/', (req,res)=>{
     res.send("<h1>Welcome to Amazon web scraper API</h1>");
 })
 
+//Get product details
+app.get('/products/:productId', async(req,res)=>{
+    const { productId } = req.params;
+
+    try {
+        const response = await request(''+baseUrl+'&url=https://www.amazon.com/dp/'+productId);
+        res.json(JSON.parse(response));
+    }
+    catch (err) {
+        res.json(err);
+
+    }
+});
+
+//Get Product Review :- Amazon link code: 404
+app.get('/products/:productId/reviews', async(req,res)=>{
+    const { productId } = req.params;
+
+    try {
+        const response = await request(''+baseUrl+'&url=https://www.amazon.com/product-reviews/'+productId);
+        res.json(JSON.parse(response));
+    }
+    catch (err) {
+        res.json(err);
+
+    }
+});
+
+//Search Product
+app.get('/search/:searchQuery', async(req,res)=>{
+    const { searchQuery } = req.params;
+
+    try {
+        const response = await request(''+baseUrl+'&url=https://www.amazon.com/s?k='+searchQuery);
+        res.json(JSON.parse(response));
+    }
+    catch (err) {
+        res.json(err);
+
+    }
+});
+
+
+
 app.listen(PORT, ()=> console.log('Server on port '+PORT));
 
