74f9213fc028daff35b9ee7230f1fe3ab578d705
Beryl-18 <beryla.s@hotmail.com>
2023-07-17 15:40:14 +0400

    fixing errors +  Creating link to scraper api

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
-const request = require('require-promise');
+const request = require('request-promise');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
+
+const apiKey = 'ee6f1250d682c98740c6f4026f57e6bc';
+const baseUrl = 'http://api.scraperapi.com?api_key='+apiKey+'&autoparse=true';
 
 
 app.use(express.json());
 
 app.get('/', (req,res)=>{
-    res.send('Welcome to Amazon web scraper API');
+    res.send("<h1>Welcome to Amazon web scraper API</h1>");
 })
 
-app.listen(PORT, ()=> console.log('Server on port ${PORT}'));
+app.listen(PORT, ()=> console.log('Server on port '+PORT));
+
